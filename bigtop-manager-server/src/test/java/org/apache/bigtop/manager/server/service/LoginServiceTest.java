ff82a89e811f077ab4a1a69110dccf10e28a53d0
timyuer <524860213@qq.com>
2026-03-30 21:38:53 +0800

    feat: implement JWT utility class with configurable properties and token management (#293)

--- bigtop-manager-server/src/test/java/org/apache/bigtop/manager/server/service/LoginServiceTest.java
+++ bigtop-manager-server/src/test/java/org/apache/bigtop/manager/server/service/LoginServiceTest.java
@@ -27,6 +27,7 @@ import org.apache.bigtop.manager.server.exception.ApiException;
 import org.apache.bigtop.manager.server.model.dto.LoginDTO;
 import org.apache.bigtop.manager.server.service.impl.LoginServiceImpl;
 import org.apache.bigtop.manager.server.utils.CacheUtils;
+import org.apache.bigtop.manager.server.utils.JWTUtils;
 import org.apache.bigtop.manager.server.utils.PasswordUtils;
 import org.apache.bigtop.manager.server.utils.Pbkdf2Utils;
 
@@ -50,6 +51,9 @@ public class LoginServiceTest {
 
     @Mock
     private UserDao userDao;
+
+    @Mock
+    private JWTUtils jwtUtils;
 
     @InjectMocks
     private LoginService loginService = new LoginServiceImpl();
@@ -107,6 +111,7 @@ public class LoginServiceTest {
         LoginDTO loginDTO = createLoginDTO(RAW_PASSWORD);
 
         when(userDao.findByUsername(any())).thenReturn(mockUser);
+        when(jwtUtils.generateToken(any(), any(), any())).thenReturn("test-token");
 
         Object result = loginService.login(loginDTO);
         assertNotNull(result);
